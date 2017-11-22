6deb2e310b575603e67300df41531dc854be9efd
Ciaran Finn <cifinn@tcd.ie>
2017-11-22 18:49:25 +0000

    implemented a temperature simulator thread

--- zephyr/cs7ns2/patient_monitor/src/sensors.c
+++ zephyr/cs7ns2/patient_monitor/src/sensors.c
@@ -5,6 +5,10 @@
 #include <misc/printk.h>
 #include "config.h"
 #include "tb_pubsub.h"
+#include <math.h>
+
+
+K_MUTEX_DEFINE(report_temp);
 
 #define BTN_PORT SW0_GPIO_NAME
 #define BTN_COUNT 4
@@ -23,6 +27,37 @@ u32_t state[4];
 
 int btn_alert_handler(struct k_alert *alert);
 K_ALERT_DEFINE(btn_alert, btn_alert_handler, 10);
+
+// configure thread
+K_THREAD_STACK_DEFINE(ss_stack_area, SS_STACK_SIZE);
+struct k_thread ss_thread;
+
+// generate fake temperature readings
+void temp_sim_thread(void * a, void * b, void * c)
+{
+	ARG_UNUSED(a);
+	ARG_UNUSED(b);
+	ARG_UNUSED(c);
+	char payload[128];
+
+	while (true) {
+		k_sleep(APP_SLEEP_MSECS);
+
+		k_mutex_lock(&report_temp, K_FOREVER);
+
+		y = sin(x * 2 * PI);
+
+		if ((x = x + X_STEP) >= 1.0)
+			x = 0.0;
+
+		printk("Temperature Reading: %d\n",y);
+		snprintf(payload, sizeof(payload), "{\"tmp\":\"%d\"}", y);
+		k_mutex_unlock(&report_temp);
+
+		tb_publish_telemetry(payload);
+	}
+}
+
 
 void btn_handler(struct device *port, struct gpio_callback *cb,
 					u32_t pins)
@@ -77,4 +112,11 @@ void sensors_start()
 		gpio_pin_enable_callback(btn_dev, btn_arr[i]);
 		gpio_pin_read(btn_dev, btn_arr[i], &state[i]);
 	}
+
+  // invoke temperature simulator
+	k_tid_t ss_tid = k_thread_create(&ss_thread, ss_stack_area,
+								 K_THREAD_STACK_SIZEOF(ss_stack_area),
+								 temp_sim_thread,
+								 NULL, NULL, NULL,
+								 SS_PRIORITY, 0, K_NO_WAIT);
 }
